14abbb6906776372eb84dcdc95b07e6a450ba098
unknown <tranhuy110421@gmail.com>
2026-03-09 12:01:26 +0700

    Initial Spring Boot Maven project

---
+++ src/main/java/com/example/caro_plus/CaroPlusApplication.java
@@ -0,0 +1,13 @@
+package com.example.caro_plus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaroPlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaroPlusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/caro_plus/CaroPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.caro_plus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaroPlusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
